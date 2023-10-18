c2991590e77fb6baa3065aab10c19d8a223a009c
Pavel Sytnik <pashasytnik@gmail.com>
2023-10-18 20:53:36 +0400

    Add image loader

---
+++ ImageLoader.cpp
@@ -0,0 +1,29 @@
+#include "ImageLoader.hpp"
+
+#include <cstring>
+
+#include "SDL2/SDL_image.h"
+
+image_t ImageLoader::loadPNG(std::string file) {
+    image_t image(nullptr, SDL_FreeSurface);
+    image.reset(IMG_Load(file.c_str()));
+    flipVertically(image);
+    return image;
+}
+
+void ImageLoader::flipVertically(image_t& image) {
+
+    Uint8* row = new Uint8[image->pitch];
+
+    for (int i = 0; i < image->h / 2; i++) {
+
+        Uint8* pixptr1 = (Uint8*)image->pixels + i * image->pitch;
+        Uint8* pixptr2 = (Uint8*)image->pixels + (image->h - 1 - i) * image->pitch;
+
+        std::memcpy(row, pixptr1, image->pitch);
+        std::memcpy(pixptr1, pixptr2, image->pitch);
+        std::memcpy(pixptr2, row, image->pitch);
+    }
+
+    delete[] row;
+}
---
+++ ImageLoader.hpp
@@ -0,0 +1,19 @@
+#ifndef IMAGE_LOADER_HPP
+#define IMAGE_LOADER_HPP
+
+#include <string>
+#include <memory>
+
+#include "SDL2/SDL.h"
+
+using image_t = std::unique_ptr<SDL_Surface, void (*)(SDL_Surface*)>;
+
+class ImageLoader {
+public:
+    ImageLoader() = delete;
+    static image_t loadPNG(std::string file);
+private:
+    static void flipVertically(image_t& image);
+};
+
+#endif
