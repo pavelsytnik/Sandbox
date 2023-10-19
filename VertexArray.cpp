27967299ce5f5d23029b532b1f0d54263f38c5ab
Pavel Sytnik <pashasytnik@gmail.com>
2023-10-19 12:39:02 +0400

    Add VAO and VBO encapsulations

---
+++ VertexArray.cpp
@@ -0,0 +1,21 @@
+#include "VertexArray.hpp"
+
+VertexArray::VertexArray() {
+    glGenVertexArrays(1, &m_id);
+}
+
+VertexArray::~VertexArray() {
+    glDeleteVertexArrays(1, &m_id);
+}
+
+GLuint VertexArray::id() {
+    return m_id;
+}
+
+void VertexArray::bind() {
+    glBindVertexArray(m_id);
+}
+
+void VertexArray::unbind() {
+    glBindVertexArray(0);
+}
---
+++ VertexBuffer.cpp
@@ -0,0 +1,37 @@
+#include "VertexBuffer.hpp"
+
+VertexBuffer::VertexBuffer() {
+    glGenBuffers(1, &m_id);
+}
+
+VertexBuffer::~VertexBuffer() {
+    glDeleteBuffers(1, &m_id);
+}
+
+GLuint VertexBuffer::id() {
+    return m_id;
+}
+
+void VertexBuffer::bind() {
+    glBindBuffer(GL_ARRAY_BUFFER, m_id);
+}
+
+void VertexBuffer::unbind() {
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
+void VertexBuffer::setData(GLsizeiptr size, const void* data, GLenum usage) {
+    //bind();
+    glBufferData(GL_ARRAY_BUFFER, size, data, usage);
+}
+
+void VertexBuffer::attachAttributePointer(
+    GLuint index,
+    GLint size,
+    GLenum type,
+    GLboolean normalized,
+    GLsizei stride,
+    const void* pointer
+) {
+    glVertexAttribPointer(index, size, type, normalized, stride, pointer);
+}
