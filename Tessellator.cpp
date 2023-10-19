3d574003d87ab7a345a42b7cd432790966fc5f70
Pavel Sytnik <pashasytnik@gmail.com>
2023-10-19 14:58:53 +0400

    Tessellator for rendering the vertex buffer

---
+++ Tessellator.cpp
@@ -0,0 +1,58 @@
+#include "Tessellator.hpp"
+
+#include <iostream>
+
+Tessellator::Tessellator() {
+    m_vao.bind();
+    m_vbo.bind();
+    //m_vbo.setData(m_vertices.size() * sizeof(GLfloat), m_vertices.data(), GL_STATIC_DRAW);
+    m_vbo.attachAttributePointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+    m_vbo.attachAttributePointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+    m_vbo.unbind();
+    m_vao.unbind();
+}
+
+void Tessellator::addVertex(GLfloat x, GLfloat y, GLfloat z) {
+    m_vertices.insert(m_vertices.end(), { x, y, z });
+}
+
+void Tessellator::addTextureCoordinate(GLfloat u, GLfloat v) {
+    m_vertices.insert(m_vertices.end(), { u, v });
+}
+
+std::size_t Tessellator::getBufferSize() {
+    return m_vertices.size();
+}
+
+void Tessellator::flush() {
+    m_vertices[0] += 0.01f;
+    m_vbo.bind();
+    m_vbo.setData(m_vertices.size() * sizeof(GLfloat), m_vertices.data(), GL_STATIC_DRAW);
+    m_vbo.unbind();
+    m_shader.use();
+
+    m_vao.bind();
+    glDrawArrays(GL_TRIANGLES, 0, m_vertices.size());
+    m_vao.unbind();
+
+    //clear();
+}
+
+void Tessellator::clear() {
+    m_vertices.clear();
+    m_vao.bind();
+    m_vbo.bind();
+    m_vbo.setData(m_vertices.size() * sizeof(GLfloat), nullptr, GL_STATIC_DRAW);
+    m_vbo.unbind();
+    m_vao.unbind();
+}
+
+GLuint Tessellator::vbo() {
+    return m_vbo.id();
+}
+
+GLuint Tessellator::vao() {
+    return m_vao.id();
+}
---
+++ Tessellator.hpp
@@ -0,0 +1,31 @@
+#ifndef TESSELLATOR_HPP
+#define TESSELLATOR_HPP
+
+#include <vector>
+
+#include "VertexBuffer.hpp"
+#include "VertexArray.hpp"
+#include "Shader.hpp"
+
+using FloatBuffer = std::vector<GLfloat>;
+
+class Tessellator {
+public:
+    Tessellator();
+    ~Tessellator() = default;
+    void flush();
+    void clear();
+    void addVertex(GLfloat x, GLfloat y, GLfloat z);
+    void addTextureCoordinate(GLfloat u, GLfloat v);
+    std::size_t getBufferSize();
+    GLuint vbo();
+    GLuint vao();
+private:
+    VertexBuffer m_vbo;
+    VertexArray m_vao;
+    Shader m_shader{ "resources/shaders/shader.vert", "resources/shaders/shader.frag" };
+
+    FloatBuffer m_vertices;
+};
+
+#endif
