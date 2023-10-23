82a66e118aecf94fb243c03506e4df62a1b5f200
Pavel Sytnik <pashasytnik@gmail.com>
2023-10-23 23:52:22 +0400

    Implement loading matrices to shader

--- BasicShader.cpp
+++ BasicShader.cpp
@@ -1,5 +1,7 @@
 #include "BasicShader.hpp"
 
+#include <glm/gtc/type_ptr.hpp>
+
 BasicShader::BasicShader(const std::string& vertexFile,
                          const std::string& fragmentFile)
     : Shader(vertexFile, fragmentFile)
@@ -11,4 +13,16 @@ void BasicShader::getUniforms() {
     m_modelMatrixLocation = glGetUniformLocation(m_id, "model");
     m_viewMatrixLocation = glGetUniformLocation(m_id, "view");
     m_projectionMatrixLocation = glGetUniformLocation(m_id, "projection");
+}
+
+void BasicShader::loadModelMatrix(const glm::mat4& model) {
+    glUniformMatrix4fv(m_modelMatrixLocation, 1, GL_FALSE, glm::value_ptr(model));
+}
+
+void BasicShader::loadViewMatrix(const glm::mat4& view) {
+    glUniformMatrix4fv(m_viewMatrixLocation, 1, GL_FALSE, glm::value_ptr(view));
+}
+
+void BasicShader::loadProjectionMatrix(const glm::mat4& projection) {
+    glUniformMatrix4fv(m_projectionMatrixLocation, 1, GL_FALSE, glm::value_ptr(projection));
 }
--- BasicShader.hpp
+++ BasicShader.hpp
@@ -3,11 +3,18 @@
 
 #include "Shader.hpp"
 
+#include <glm/glm.hpp>
+//#include <glm/gtc/type_ptr.hpp>
+
 class BasicShader : public Shader {
 
 public:
     BasicShader(const std::string& vertexFile = "basic.vert",
                 const std::string& fragmentFile = "basic.frag");
+
+    void loadModelMatrix(const glm::mat4& model);
+    void loadViewMatrix(const glm::mat4& view);
+    void loadProjectionMatrix(const glm::mat4& projection);
 
 protected:
     virtual void getUniforms() override;
