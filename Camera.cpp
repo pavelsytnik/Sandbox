19371183235da90b4ec06e827f24a8ebff7c4466
Pavel Sytnik <pashasytnik@gmail.com>
2023-10-25 18:28:46 +0400

    Update Camera class

--- Camera.cpp
+++ Camera.cpp
@@ -5,7 +5,9 @@
 Camera::Camera() :
     m_position{0.f, 0.f, 0.f},
     m_direction{0.f, 0.f, -1.f},
-    m_axis{0.f, 1.f, 0.f}
+    m_axis{0.f, 1.f, 0.f},
+    m_viewMatrix{glm::lookAt(m_position, m_position + m_direction, m_axis)},
+    m_projectionMatrix{glm::perspective(glm::radians(70.f), 1.f, 0.1f, 20.f)}
 {
 }
 
@@ -25,6 +27,10 @@ const glm::vec3& Camera::getDirection() const {
     return m_direction;
 }
 
-glm::mat4 Camera::getViewMatrix() const {
-    return glm::lookAt(m_position, m_position + m_direction, m_axis);
+const glm::mat4& Camera::getViewMatrix() const {
+    return m_viewMatrix;
+}
+
+const glm::mat4& Camera::getProjectionMatrix() const {
+    return m_projectionMatrix;
 }
--- Camera.hpp
+++ Camera.hpp
@@ -7,16 +7,22 @@ class Camera {
 
 public:
     Camera();
+
     void setPosition(const glm::vec3& position);
     void setDirection(const glm::vec3& direction);
     const glm::vec3& getPosition() const;
     const glm::vec3& getDirection() const;
-    glm::mat4 getViewMatrix() const;
+
+    const glm::mat4& getViewMatrix() const;
+    const glm::mat4& getProjectionMatrix() const;
 
 private:
     glm::vec3 m_position;
     glm::vec3 m_direction;
     glm::vec3 m_axis;
+
+    glm::mat4 m_viewMatrix;
+    glm::mat4 m_projectionMatrix;
 };
 
 #endif
