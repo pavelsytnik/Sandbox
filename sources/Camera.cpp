5bf0a19278337b1cda235355ed46a6e2c2986ac4
Pavel Sytnik <pashasytnik@gmail.com>
2023-10-31 22:14:22 +0400

    Expanded camera interface

--- sources/Camera.cpp
+++ sources/Camera.cpp
@@ -2,26 +2,36 @@
 
 #include <glm/gtc/matrix_transform.hpp>
 
-Camera::Camera() :
-    m_position{5.f, 5.f, 15.f},
+Camera::Camera(glm::float32 ratio) :
+    m_position{0.f, 0.f, 0.f},
     m_direction{0.f, 0.f, -1.f},
     m_axis{0.f, 1.f, 0.f},
+    m_ratio{ratio},
+    m_fov{glm::radians(70.f)},
     m_viewMatrix{glm::lookAt(m_position, m_position + m_direction, m_axis)},
-    m_projectionMatrix{glm::perspective(glm::radians(70.f), 1.f, 0.1f, 100.f)}
+    m_projectionMatrix{glm::perspective(m_fov, m_ratio, 0.1f, 100.f)}
 {
     //m_viewMatrix = glm::rotate(m_viewMatrix, glm::radians(60.f), glm::vec3(0, 1, 0));
 }
 
 void Camera::setPosition(const glm::vec3& position) {
     m_position = position;
+    m_viewMatrix = glm::lookAt(m_position, m_position + m_direction, m_axis);
 }
 
 void Camera::setDirection(const glm::vec3& direction) {
     m_direction = glm::normalize(direction);
+    m_viewMatrix = glm::lookAt(m_position, m_position + m_direction, m_axis);
 }
 
 void Camera::setRatio(glm::float32 ratio) {
-    m_projectionMatrix = glm::perspective(glm::radians(70.f), ratio, 0.1f, 100.f);
+    m_ratio = ratio;
+    m_projectionMatrix = glm::perspective(m_fov, m_ratio, 0.1f, 100.f);
+}
+
+void Camera::setFOV(glm::float32 fov) {
+    m_fov = fov;
+    m_projectionMatrix = glm::perspective(m_fov, m_ratio, 0.1f, 100.f);
 }
 
 const glm::vec3& Camera::getPosition() const {
@@ -30,6 +40,14 @@ const glm::vec3& Camera::getPosition() const {
 
 const glm::vec3& Camera::getDirection() const {
     return m_direction;
+}
+
+glm::float32 Camera::getRatio() const {
+    return m_ratio;
+}
+
+glm::float32 Camera::getFOV() const {
+    return m_fov;
 }
 
 const glm::mat4& Camera::getViewMatrix() const {
--- sources/Camera.hpp
+++ sources/Camera.hpp
@@ -6,22 +6,28 @@
 class Camera {
 
 public:
-    Camera();
+    Camera(glm::float32 ratio = 1.f);
 
     void setPosition(const glm::vec3& position);
     void setDirection(const glm::vec3& direction);
     const glm::vec3& getPosition() const;
     const glm::vec3& getDirection() const;
 
+    void setFOV(glm::float32 fov);
+    void setRatio(glm::float32 ratio);
+    glm::float32 getFOV() const;
+    glm::float32 getRatio() const;
+
     const glm::mat4& getViewMatrix() const;
     const glm::mat4& getProjectionMatrix() const;
-
-    void setRatio(glm::float32 ratio);
 
 private:
     glm::vec3 m_position;
     glm::vec3 m_direction;
     glm::vec3 m_axis;
+
+    glm::float32 m_fov;
+    glm::float32 m_ratio;
 
     glm::mat4 m_viewMatrix;
     glm::mat4 m_projectionMatrix;
