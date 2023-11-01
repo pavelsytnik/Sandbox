ddf30c57bc9029ebbbcc81d144a901c4c00a69bd
Pavel Sytnik <pashasytnik@gmail.com>
2023-11-01 20:52:59 +0400

    Fix KeyMappings class

--- sources/Registry/KeyMappings.cpp
+++ sources/Registry/KeyMappings.cpp
@@ -1,6 +1,10 @@
 #include "KeyMappings.hpp"
 #include "Registry.hpp"
 
+#include <iostream>
+
+KeyMappings* KeyMappings::m_instance;
+
 KeyMappings::KeyMappings() :
     FORWARD(Registry::registerKey({SDL_SCANCODE_W})),
     BACKWARD(Registry::registerKey({SDL_SCANCODE_S})),
@@ -15,7 +19,9 @@ KeyMappings::KeyMappings() :
 KeyMappings& KeyMappings::getInstance() {
     if (m_instance == nullptr) {
         m_instance = new KeyMappings;
+        //std::cout << "instance\n";
     }
+    return *m_instance;
 }
 
 void KeyMappings::deleteInstance() {
