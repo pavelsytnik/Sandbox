849a834755239ff5b0c82f874e0c4def39a26417
Pavel Sytnik <pashasytnik@gmail.com>
2023-11-06 22:54:20 +0400

    Add methods to Entity

--- sources/Entity/Entity.cpp
+++ sources/Entity/Entity.cpp
@@ -1,7 +1,28 @@
 #include "Entity.hpp"
 
-Entity::Entity(World& world) : m_world(world) {
+#include "../World/World.hpp"
 
+Entity::Entity(World& world) :
+    m_position(glm::vec3(world.getXSize() / 2, world.getYSize() + 2, world.getZSize() / 2)),
+    m_yaw(270.f),
+    m_world(world) {
+
+}
+
+float Entity::getYaw() const {
+    return m_yaw;
+}
+
+float Entity::getPitch() const {
+    return m_pitch;
+}
+
+const glm::vec3& Entity::getAcceleration() const {
+    return m_acceleration;
+}
+
+const glm::vec3& Entity::getPosition() const {
+    return m_position;
 }
 
 void Entity::move(std::uint64_t dt) {
--- sources/Entity/Entity.hpp
+++ sources/Entity/Entity.hpp
@@ -7,16 +7,26 @@
 class World;
 
 class Entity {
+
+    friend class World;
     
 public:
     Entity(World& world);
     void move(std::uint64_t dt);
 
+    float getYaw() const;
+    float getPitch() const;
+
+    const glm::vec3& getPosition() const;
+    const glm::vec3& getAcceleration() const;
+
 protected:
     glm::vec3 m_position;
+    glm::vec3 m_acceleration;
+
     float m_yaw;
     float m_pitch;
-    glm::vec3 m_acceleration;
+
     World& m_world;
 };
 
