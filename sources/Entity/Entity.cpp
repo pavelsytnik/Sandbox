6852f4974fa989a528e027a261dc33d102c1a56b
Pavel Sytnik <pashasytnik@gmail.com>
2023-11-11 23:18:33 +0400

    Refactor Entity a little

--- sources/Entity/Entity.cpp
+++ sources/Entity/Entity.cpp
@@ -5,13 +5,15 @@
 
 #include "../World/World.hpp"
 
+#include <glm/gtx/vector_angle.hpp>
+
 Entity::Entity(World& world) :
-    //m_position(glm::vec3(world.getXSize() / 2, world.getYSize() + 2, world.getZSize() / 2)),
+    m_position(0.f),
     m_velocity(0.f),
     m_force(0.f),
-    m_inertia(0.f),
     m_impulse(false),
     m_yaw(270.f),
+    m_pitch(0.f),
     m_world(world) {
 
 }
@@ -23,10 +25,6 @@ float Entity::getYaw() const {
 float Entity::getPitch() const {
     return m_pitch;
 }
-
-//const glm::vec3& Entity::getAcceleration() const {
-    //return m_acceleration;
-//}
 
 const glm::vec3& Entity::getVelocity() const {
     return m_velocity;
@@ -53,15 +51,13 @@ void Entity::setPosition(const glm::vec3& position) {
 }
 
 void Entity::setMotion(const glm::vec3& direction) {
+
     if (glm::length(direction) != 0) {
         m_impulse = glm::length(m_force) == 0;
         m_force = glm::normalize(direction);
-        //m_inertia = glm::length(m_velocity) != 0 ? glm::normalize(m_velocity) : glm::vec3(0.f);
-        //m_impulse = true;
-    }
-    else {
+    } else {
+        m_impulse = false;
         m_force = glm::vec3(0.f);
-        //m_impulse = false;
     }
 }
 
@@ -70,21 +66,16 @@ void Entity::move(std::uint64_t dt) {
     auto k = 0.5f;
     float delta = dt / 1000000.f / 1000.f;
 
-    static float n = 0.f;
-
-    //std::cout << glm::length(m_force) << std::endl;
-    //std::cout << glm::length(m_velocity) << std::endl << std::endl;
-
-
-    m_position += delta * 15 * m_velocity;
-
     glm::vec3 dif = m_velocity - m_force;
-    std::cout << dif.x << ", " << dif.y << ", " << dif.z << std::endl;
 
     if (m_impulse && glm::length(m_velocity) == 0) {
         m_velocity = m_force;
+
     } else if (m_impulse) {
-        m_velocity = glm::mix(m_velocity, m_force, 0.2f);
+        m_velocity = glm::mix(m_velocity, m_force, std::min(1.f, 0.5f / glm::length(dif)));
+    // or
+    //  m_velocity = glm::mix(m_velocity, m_force, 0.2f);
+
     } else {
 
         if (glm::length(dif) != 0.f && glm::length(dif) - delta * k >= 0.f) {
@@ -92,7 +83,10 @@ void Entity::move(std::uint64_t dt) {
         } else {
             dif = glm::vec3(0.f);
         }
+
         m_velocity = m_force + dif;
     }
-    //m_impulse = false;
+
+    m_position += delta * 10 * m_velocity;
+    m_impulse = false;
 }
--- sources/Entity/Entity.hpp
+++ sources/Entity/Entity.hpp
@@ -7,8 +7,6 @@
 class World;
 
 class Entity {
-
-    //friend class World;
     
 public:
     Entity(World& world);
@@ -18,7 +16,6 @@ public:
     float getPitch() const;
 
     const glm::vec3& getPosition() const;
-    //const glm::vec3& getAcceleration() const;
     const glm::vec3& getForce() const;
     const glm::vec3& getVelocity() const;
 
@@ -35,13 +32,9 @@ public:
 
 protected:
     glm::vec3 m_position;
-    //glm::vec3 m_acceleration;
     glm::vec3 m_velocity;
     glm::vec3 m_force;
 
-    glm::vec3 m_inertia;
-    
-    //float m_speed;
     bool m_impulse;
 
     float m_yaw;
