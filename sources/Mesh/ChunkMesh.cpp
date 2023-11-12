48fb5e777bf31e7123ab6b3648af29775033d1bd
Pavel Sytnik <pashasytnik@gmail.com>
2023-11-12 22:20:14 +0400

    Now block position is passed in addFace; Make type aliases for addFace's parameters 'face' and 'texture'

--- sources/Mesh/ChunkMesh.cpp
+++ sources/Mesh/ChunkMesh.cpp
@@ -1,12 +1,13 @@
 #include "ChunkMesh.hpp"
 
-void ChunkMesh::addFace(const std::array<GLfloat, 18>& face,
-                        const std::array<GLfloat, 12>& texture)
+void ChunkMesh::addFace(const Face& face,
+                        const TextureAtlasCoord& texture,
+                        std::uint32_t x, std::uint32_t y, std::uint32_t z)
 {
     for (int i = 0; i < 6; ++i) {
         int faceShift = i * 3;
         int texShift = i * 2;
-        addVertex({face[faceShift], face[faceShift + 1], face[faceShift + 2]},
+        addVertex({face[x + faceShift], face[y + faceShift + 1], face[z + faceShift + 2]},
                   {texture[texShift], texture[texShift + 1]});
     }
 }
--- sources/Mesh/ChunkMesh.hpp
+++ sources/Mesh/ChunkMesh.hpp
@@ -6,12 +6,17 @@
 #include "Mesh.hpp"
 #include "../Texture/Texture.hpp"
 
+using Face = std::array<GLfloat, 18>;
+using TextureAtlasCoord = std::array<GLfloat, 12>;
+
 class ChunkMesh : public Mesh {
 
 public:
     ChunkMesh() = default;
 
-    void addFace(const std::array<GLfloat, 18>& face, const std::array<GLfloat, 12>& texture);
+    void addFace(const Face& face,
+                 const TextureAtlasCoord& texture,
+                 std::uint32_t x,std::uint32_t y, std::uint32_t z);
 };
 
 #endif
