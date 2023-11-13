f73cf0093286ad06720540290634a00544920c6c
Pavel Sytnik <pashasytnik@gmail.com>
2023-11-13 12:11:03 +0400

    Add list of resource folders as constants

---
+++ sources/Util/Folders.hpp
@@ -0,0 +1,13 @@
+#ifndef FOLDERS_HPP
+#define FOLDERS_HPP
+
+#include <string>
+
+namespace folders {
+
+    constexpr std::string resources("resources/");
+    constexpr std::string shaders(resources + "shaders/");
+    constexpr std::string textures(resources + "images/");
+}
+
+#endif
