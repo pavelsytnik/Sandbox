784cfa8e1cf0f7066529f4db3973c61c429d5ba2
Pavel Sytnik <pashasytnik@gmail.com>
2023-11-13 23:38:38 +0400

    Fixed mistake with wrong indexation and therefore coordinate calculation

--- sources/Mesh/ChunkMesh.cpp
+++ sources/Mesh/ChunkMesh.cpp
@@ -7,7 +7,7 @@ void ChunkMesh::addFace(const Face& face,
     for (int i = 0; i < 6; ++i) {
         int faceShift = i * 3;
         int texShift = i * 2;
-        addVertex({face[x + faceShift], face[y + faceShift + 1], face[z + faceShift + 2]},
+        addVertex({x + face[faceShift], y + face[faceShift + 1], z + face[faceShift + 2]},
                   {texture[texShift], texture[texShift + 1]});
     }
 }
