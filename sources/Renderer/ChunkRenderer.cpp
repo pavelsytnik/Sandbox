fff4450bb67bb4817f7d85326603ed295eb598d1
Pavel Sytnik <pashasytnik@gmail.com>
2023-11-13 23:35:28 +0400

    Implement chunk renderer

--- sources/Renderer/ChunkRenderer.cpp
+++ sources/Renderer/ChunkRenderer.cpp
@@ -1,9 +1,25 @@
 #include "ChunkRenderer.hpp"
+#include "../Camera.hpp"
 
-void ChunkRenderer::add(const ChunkMesh& mesh)
+void ChunkRenderer::add(const std::shared_ptr<ChunkMesh>& mesh)
 {
+    m_meshes.push_back(mesh);
 }
 
-void ChunkRenderer::render()
+void ChunkRenderer::clear()
 {
+    m_meshes.clear();
+}
+
+void ChunkRenderer::render(const Camera& camera)
+{
+    m_shader.use();
+    m_atlas.bind();
+
+    m_shader.loadModelMatrix(glm::mat4(1.f));
+    m_shader.loadViewMatrix(camera.getViewMatrix());
+    m_shader.loadProjectionMatrix(camera.getProjectionMatrix());
+
+    for (auto& mesh : m_meshes)
+        mesh.lock()->draw();
 }
--- sources/Renderer/ChunkRenderer.hpp
+++ sources/Renderer/ChunkRenderer.hpp
@@ -1,16 +1,26 @@
 #ifndef CHUNK_RENDERER_HPP
 #define CHUNK_RENDERER_HPP
 
-class ChunkMesh;
+#include <vector>
+#include <memory>
+
+#include "../Shader/BasicShader.hpp"
+#include "../Mesh/ChunkMesh.hpp"
+#include "../Texture/Texture.hpp"
+
+class Camera;
 
 class ChunkRenderer {
 
 public:
-    void add(const ChunkMesh& mesh);
-    void render();
+    void add(const std::shared_ptr<ChunkMesh>& mesh);
+    void clear();
+    void render(const Camera& camera);
 
 private:
-    // TODO: ChunkMesh info for rendering
+    BasicShader m_shader;
+    std::vector<std::weak_ptr<ChunkMesh>> m_meshes; // Possibly bad idea
+    Texture m_atlas;
 };
 
 #endif
