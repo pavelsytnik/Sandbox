0c8a28fe57805bdc5a939c9f3c1996ad55867dee
Pavel Sytnik <pashasytnik@gmail.com>
2023-11-15 21:19:58 +0400

    Paths.hpp: Add namespace 'files'

--- sources/Util/Paths.hpp
+++ sources/Util/Paths.hpp
@@ -1,5 +1,5 @@
-#ifndef FOLDERS_HPP
-#define FOLDERS_HPP
+#ifndef PATHS_HPP
+#define PATHS_HPP
 
 // TODO: implement constexpr string literal concatenation
 
@@ -8,6 +8,17 @@ namespace folders {
     constexpr auto resources = "resources/";
     constexpr auto shaders   = "resources/shaders/";
     constexpr auto textures  = "resources/images";
+}
+
+namespace files {
+    
+    constexpr auto blockAtlas          = "resources/images/blocks.png";
+
+    constexpr auto basicVertexShader   = "resources/shaders/basic.vert";
+    constexpr auto basicFragmentShader = "resources/shaders/basic.frag";
+
+    constexpr auto chunkVertexShader   = "resources/shaders/chunk.vert";
+    constexpr auto chunkFragmentShader = "resources/shaders/chunk.frag";
 }
 
 #endif
