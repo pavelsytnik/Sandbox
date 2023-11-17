338104f3c4f4801a178c511c07eecf96e410015e
Pavel Sytnik <pashasytnik@gmail.com>
2023-11-17 19:05:48 +0400

    Faces.hpp: Now they contain unique vertices only

--- sources/Mesh/Faces.hpp
+++ sources/Mesh/Faces.hpp
@@ -6,12 +6,12 @@
 
 namespace faces {
 
-    constexpr std::array<GLfloat, 18> back   = {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0};
-    constexpr std::array<GLfloat, 18> front  = {0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1};
-    constexpr std::array<GLfloat, 18> left   = {0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1};
-    constexpr std::array<GLfloat, 18> right  = {1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1};
-    constexpr std::array<GLfloat, 18> bottom = {0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0};
-    constexpr std::array<GLfloat, 18> top    = {0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0};
+    constexpr std::array<GLfloat, 12> back   = {0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0};
+    constexpr std::array<GLfloat, 12> front  = {0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1};
+    constexpr std::array<GLfloat, 12> left   = {0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0};
+    constexpr std::array<GLfloat, 12> right  = {1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0};
+    constexpr std::array<GLfloat, 12> bottom = {0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1};
+    constexpr std::array<GLfloat, 12> top    = {0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1};
 }
 
 #endif
