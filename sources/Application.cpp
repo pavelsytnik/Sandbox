7d36d15b344ca2dfa40e56e536a11e32065bfd7b
Pavel Sytnik <pashasytnik@gmail.com>
2023-11-19 23:52:55 +0400

    Make fields pointers; add Window abstraction

--- sources/Application.cpp
+++ sources/Application.cpp
@@ -3,9 +3,12 @@
 #include <iostream>
 
 Application::Application() :
-    m_context{}, // load Registry also
+    m_sdlHolder{new SDLHolder()},
     m_running{false},
     m_visible{true},
+    m_window{new Window(*m_sdlHolder->getWindow())},
+    m_keyboard{new Keyboard()},
+    m_mouse{new Mouse()},
     m_state{}
 {
     m_state = std::make_unique<PlayingState>(*this);
@@ -24,7 +27,7 @@ void Application::run() {
         }
         m_state->update(deltaTime);
         if (m_visible) {
-            m_renderer.swap(m_context.getWindow(), *m_state);
+            m_renderer->swap(*m_window, *m_state);
         }
 
         deltaTime = frameStart.restart();
@@ -32,40 +35,48 @@ void Application::run() {
 }
 
 const Keyboard& Application::getKeyboard() const {
-    return m_keyboard;
+    return *m_keyboard;
+}
+
+const Window& Application::getWindow() const {
+    return *m_window;
 }
 
 const Mouse& Application::getMouse() const {
-    return m_mouse;
+    return *m_mouse;
 }
 
 bool Application::isRunning() const {
     return m_running;
 }
 
-Context& Application::getContext() {
-    return m_context;
-}
+//Context& Application::getContext() {
+//    return m_context;
+//}
 
 void Application::handleEvents() {
 
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
-        m_state->handleEvent(event);
+
         if (event.type == SDL_QUIT) {
             m_running = false;
         } else if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) {
-            m_keyboard.handleInput(event.key);
+            m_keyboard->handleInput(event.key);
         } else if (event.type == SDL_MOUSEMOTION) {
-            m_mouse.handleEvent(event.motion);
+            m_mouse->handleEvent(event.motion);
         } else if (event.type == SDL_MOUSEBUTTONDOWN || event.type == SDL_MOUSEBUTTONUP) {
-            m_mouse.handleEvent(event.button);
+            m_mouse->handleEvent(event.button);
+        } else if (event.type == SDL_WINDOWEVENT) {
+            m_window->handleEvent(event.window);
         }
+        m_state->handleEvent(event);
     }
 
-    if (SDL_GetWindowFlags(m_context.getWindow()) & SDL_WINDOW_MINIMIZED && m_visible) {
+
+    if (SDL_GetWindowFlags(m_sdlHolder->getWindow()) & SDL_WINDOW_MINIMIZED && m_visible) {
         m_visible = false;
-    } else if (!(SDL_GetWindowFlags(m_context.getWindow()) & SDL_WINDOW_MINIMIZED) && !m_visible) {
+    } else if (!(SDL_GetWindowFlags(m_sdlHolder->getWindow()) & SDL_WINDOW_MINIMIZED) && !m_visible) {
         m_visible = true;
     }
 }
--- sources/Application.hpp
+++ sources/Application.hpp
@@ -3,13 +3,14 @@
 
 #include <memory>
 
-#include "Context.hpp"
+#include "SDLHolder.hpp"
 #include "State/State.hpp"
 #include "State/PlayingState.hpp"
 #include "Input/Keyboard.hpp"
 #include "Input/Mouse.hpp"
 #include "Util/Clock.hpp"
 #include "Renderer/RenderMaster.hpp"
+#include "Window.hpp"
 
 class Application {
 
@@ -17,22 +18,24 @@ public:
     Application();
     void run();
     bool isRunning() const;
-    Context& getContext();
+    //Context& getContext();
 
     const Mouse& getMouse() const;
     const Keyboard& getKeyboard() const;
+    const Window& getWindow() const;
 
 private:
     void handleEvents();
 
-    Context m_context;
+    SDLHolder* m_sdlHolder;
     bool m_running;
     bool m_visible;
 
-    Keyboard m_keyboard;
-    Mouse m_mouse;
+    Window* m_window;
+    Keyboard* m_keyboard;
+    Mouse* m_mouse;
 
-    RenderMaster m_renderer;
+    RenderMaster* m_renderer;
 
     std::unique_ptr<State> m_state;
 };
