0aee46ec6ca6c3b12828e1249a7d17ca58e9b997
Pavel Sytnik <pashasytnik@gmail.com>
2023-11-28 20:55:56 +0400

    Create wrapper over standard C++ tools for time representation

---
+++ sources/Util/Time.cpp
@@ -0,0 +1,29 @@
+#include "Time.hpp"
+
+constexpr Time Time::nanoseconds(std::int64_t ns) {
+    return std::chrono::nanoseconds(ns);
+}
+
+constexpr Time Time::milliseconds(std::int32_t ms) {
+    return std::chrono::milliseconds(ms);
+}
+
+constexpr Time Time::seconds(float s) {
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::duration<float>(s));
+}
+
+constexpr std::int64_t Time::asNanoseconds() const {
+    return m_nanoseconds.count();
+}
+
+constexpr std::int32_t Time::asMilliseconds() const {
+    return std::chrono::duration_cast<std::chrono::duration<std::int32_t, std::milli>>(m_nanoseconds).count();
+}
+
+constexpr float Time::asSeconds() const {
+    return std::chrono::duration<float>(m_nanoseconds).count();
+}
+
+constexpr Time::Time(std::chrono::nanoseconds nanoseconds) :
+    m_nanoseconds(nanoseconds)
+{}
---
+++ sources/Util/Time.hpp
@@ -0,0 +1,24 @@
+#ifndef TIME_HPP
+#define TIME_HPP
+
+#include <cstdint>
+#include <chrono>
+
+class Time {
+    
+public:
+    static constexpr Time nanoseconds(std::int64_t ns);
+    static constexpr Time milliseconds(std::int32_t ms);
+    static constexpr Time seconds(float s);
+
+    constexpr std::int64_t asNanoseconds() const;
+    constexpr std::int32_t asMilliseconds() const;
+    constexpr float asSeconds() const;
+
+private:
+    constexpr Time(std::chrono::nanoseconds nanoseconds);
+
+    std::chrono::nanoseconds m_nanoseconds;
+};
+
+#endif
