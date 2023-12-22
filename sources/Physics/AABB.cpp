3c23e005ed118f1549b796e3187b3ec0106d5360
Pavel Sytnik <pashasytnik@gmail.com>
2023-12-22 13:39:40 +0400

    Add AABB for collision check

---
+++ sources/Physics/AABB.cpp
@@ -0,0 +1,69 @@
+#include "AABB.hpp"
+
+// Possible through-block movement when we have bigger values of move distance. It's necessary to check
+
+float AABB::clipXCollide(const AABB& other, float x) const {
+
+    if (this->minY >= other.maxY || this->maxY <= other.minY ||
+        this->minZ >= other.maxZ || this->maxZ <= other.minZ)
+    {
+        return x;
+    }
+
+    if (x > 0.f && this->maxX < other.minX &&
+        this->maxX + x - other.minX > 0)
+    {
+        return x - (this->maxX + x - other.minX);
+    }
+    if (x < 0.f && this->minX > other.maxX
+             && other.maxX - (this->minX + x) > 0)
+    {
+        return x + (other.maxX - (this->minX + x));
+    }
+
+    return x;
+}
+
+float AABB::clipYCollide(const AABB& other, float y) const {
+
+    if (this->minX >= other.maxX || this->maxX <= other.minX ||
+        this->minZ >= other.maxZ || this->maxZ <= other.minZ)
+    {
+        return y;
+    }
+
+    if (y > 0.f && this->maxY < other.minY &&
+        this->maxY + y - other.minY > 0)
+    {
+        return y - (this->maxY + y - other.minY);
+    }
+    if (y < 0.f && this->minY > other.maxY
+        && other.maxY - (this->minY + y) > 0)
+    {
+        return y + (other.maxY - (this->minY + y));
+    }
+
+    return y;
+}
+
+float AABB::clipZCollide(const AABB& other, float z) const {
+
+    if (this->minX >= other.maxX || this->maxX <= other.minX ||
+        this->minY >= other.maxY || this->maxY <= other.minY)
+    {
+        return z;
+    }
+
+    if (z > 0.f && this->maxZ < other.minZ &&
+        this->maxZ + z - other.minZ > 0)
+    {
+        return z - (this->maxZ + z - other.minZ);
+    }
+    if (z < 0.f && this->minZ > other.maxZ
+        && other.maxZ - (this->minZ + z) > 0)
+    {
+        return z + (other.maxZ - (this->minZ + z));
+    }
+
+    return z;
+}
---
+++ sources/Physics/AABB.hpp
@@ -0,0 +1,19 @@
+#ifndef AABB_HPP
+#define AABB_HPP
+
+struct AABB {
+
+    float minX;
+    float minY;
+    float minZ;
+
+    float maxX;
+    float maxY;
+    float maxZ;
+
+    float clipXCollide(const AABB& other, float x) const;
+    float clipYCollide(const AABB& other, float y) const;
+    float clipZCollide(const AABB& other, float z) const;
+};
+
+#endif
